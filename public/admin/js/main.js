d0a9324920a3004548e33c807e34511ff39e08b5
Ede Success <edesuccess54@gmail.com>
2022-12-02 22:24:42 +0100

    fixing modal

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,8 +1,6 @@
-
 const hamburger = document.querySelector('.openNav')
 const closeNav = document.querySelector('.closeNav')
 const overlay = document.querySelector('.overlay')
-// const trash = document.querySelector('#myTable')
 const destopTabs = document.querySelector('.tabs')
 const mobileTabs = document.querySelector('.mobile-tabs')
 const profileTabContent = document.querySelector('.single-content')
@@ -11,7 +9,6 @@ const navProfile = document.querySelector('.nav-profile')
 const navSecurity = document.querySelector('.nav-security')
 const editProfileBtn = document.querySelector('.edit-profile');
 const closeModalBtn = document.querySelector('.close-modal')
-
 
 
 // function to open side nav 
@@ -50,15 +47,28 @@ destopTabs.addEventListener('click', switchTabs)
 
 // function to open modal 
 const openModal = (e) => {
+    if(e.target.classList.contains('edit')) {    
     const profileModel = document.querySelector('.profile-modal')
+    const modalOverlay = document.querySelector('.modal-overlay')
+    
+    modalOverlay.classList.add('active')
     profileModel.classList.add('show')
+    }
+
+    if(e.target.classList.contains('pwd')) {    
+        const profileModel = document.querySelector('.profile-modal')
+        const modalOverlay = document.querySelector('.modal-overlay')
+        
+        modalOverlay.classList.add('active')
+        profileModel.classList.add('show')
+        }
 }
 editProfileBtn.addEventListener('click', openModal)
 
 // function to close edit profile modal 
 const closeModal = (e) => {
     const profileModel = document.querySelector('.profile-modal')
-    document.querySelector('.modal-overlay').classList.add('active')
+    document.querySelector('.modal-overlay').classList.remove('active')
     profileModel.classList.remove('show')
 }
 closeModalBtn.addEventListener('click', closeModal)
