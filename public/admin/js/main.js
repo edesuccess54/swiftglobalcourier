e322f48b3e448c9ca89df7e6bf153a7ef222cbcd
Ede Success <edesuccess54@gmail.com>
2022-12-02 17:02:41 +0100

    changes made "

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,8 +1,17 @@
 
 const hamburger = document.querySelector('.openNav')
 const closeNav = document.querySelector('.closeNav')
-const sidebar = document.querySelector('.mobileMenu')
 const overlay = document.querySelector('.overlay')
+// const trash = document.querySelector('#myTable')
+const destopTabs = document.querySelector('.tabs')
+const mobileTabs = document.querySelector('.mobile-tabs')
+const profileTabContent = document.querySelector('.single-content')
+const securityTabContent = document.querySelector('.security')
+const navProfile = document.querySelector('.nav-profile')
+const navSecurity = document.querySelector('.nav-security')
+const editProfileBtn = document.querySelector('.edit-profile');
+const closeModalBtn = document.querySelector('.close-modal')
+
 
 
 // function to open side nav 
@@ -10,14 +19,49 @@ const openSideNav = (e) => {
     sidebar.classList.add('active')
     overlay.classList.add('active')
 }
+hamburger.addEventListener('click', openSideNav)
 
 // function top close side nav 
 const closeSideNav = (e) => {
     sidebar.classList.remove('active')
     overlay.classList.remove('active')
 }
-
-
-hamburger.addEventListener('click', openSideNav)
 overlay.addEventListener('click', closeSideNav)
 closeNav.addEventListener('click', closeSideNav)
+
+// function to switch tabs 
+const switchTabs = (e) => {
+    if(e.target.textContent === 'Profile') {
+        securityTabContent.classList.remove('active')
+        profileTabContent.classList.add('active')
+        navSecurity.classList.remove('active')
+        e.target.classList.add('active')
+    }
+
+    if(e.target.textContent === 'Security') {
+        profileTabContent.classList.remove('active')
+        securityTabContent.classList.add('active')
+        navProfile.classList.remove('active')
+        e.target.classList.add('active')
+    }
+}
+mobileTabs.addEventListener('click', switchTabs)
+destopTabs.addEventListener('click', switchTabs)
+
+// function to open modal 
+const openModal = (e) => {
+    const profileModel = document.querySelector('.profile-modal')
+    profileModel.classList.add('show')
+    overlay.classList.add('active')
+}
+editProfileBtn.addEventListener('click', openModal)
+
+// function to close edit profile modal 
+const closeModal = (e) => {
+    const profileModel = document.querySelector('.profile-modal')
+    profileModel.classList.remove('show')
+
+}
+
+
+
