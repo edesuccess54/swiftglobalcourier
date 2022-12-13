288cee0b7abd7b22ccbc46f7e07b23e4b3c15a49
Ede Success <edesuccess54@gmail.com>
2022-12-13 23:55:31 +0100

    changes made

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,46 @@
 const jwt = require('jsonwebtoken');
 const Admin = require('../models/adminModel')
+const ErrorResponse = require("../utils/errorResponse")
+
 const proctect = async (req, res, next) => {
     try {
         const token = req.cookies.token
+        console.log("token: " + token)
 
+        console.log(11)
         if(!token) {
-            res.status(401)
-            throw new Error('Not authorized, Please log in')
+            console.log("before")
+            next(new ErrorResponse("Not authorized, Please log in",401))
+            console.log("after")
+            return
         }
 
+        console.log(22)
         // verify token 
         const verified = jwt.verify(token, process.env.JWT_SECRET)
-
+        console.log('the verfified is now is -- '+verified)
+        console.log(33)
         if(!verified) {
-            res.status(400)
-            throw new Error('Invalid token')
+            next(new ErrorResponse("Invalid token",401))
+            return
         }
 
+        console.log(44)
         // get admin 
         const admin = await Admin.findById(verified.id).select('-password')
-
+        console.log(55)
         if(!admin) {
-            res.status(401)
-            throw new Error('Your account is not authorized')
+            next(new ErrorResponse("Your account is not authorized",401))
+            return
         }
-
+        console.log(66)
         req.admin = admin
         next()
         
     } catch (error) {
-        res.status(401).json({error: error.message});
+        console.log(error)
+        return res.status(401).json({error: error.message})
+        // next(new ErrorResponse({error: error.message}, 401))
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv').config()
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const adminRoutes = require('./routes/adminRoutes');
 const mainRoutes = require('./routes/mainRoutes')
 const cookieParser = require('cookie-parser')
@@ -19,8 +20,16 @@ app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(cors())
 
+app.use(morgan('dev'))
+
 app.use(mainRoutes)
 app.use('/',adminRoutes)
+
+app.use((req, res, next) => {
+   const error = new Error("Not Found");
+   error.status = 404;
+   next(error);
+})
 
 app.use(errorHandler)
 
