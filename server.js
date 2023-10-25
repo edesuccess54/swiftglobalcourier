415fdef351d10c890ab4cb36d893fe351fc77534
Ede Success <edesuccess54@gmail.com>
2023-10-25 02:39:42 +0100

    updated mongodb connection

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ app.use('*', (req, res) => {
 app.use(errorHandler)
 
 // connect to mongoDB and start server
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
 .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Connected to database and now Listening on port ${process.env.PORT}`)
