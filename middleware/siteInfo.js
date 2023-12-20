c96fab26bc737385c456d1d8a51b539bf9a1b09e
Ede Success <edesuccess54@gmail.com>
2023-12-21 00:46:14 +0100

    updated database

--- middleware/siteInfo.js
+++ middleware/siteInfo.js
@@ -1,10 +1,10 @@
 const siteInfo = async (req, res, next) => {
-    res.locals.siteName = "Crestnet Logistics and Courier Services"; 
-    res.locals.siteEmail = "info@crestnetlogistics.com";
-    res.locals.deliveryEmail = "delivery@crestnetlogistics.com";
+    res.locals.siteName = "Zegnet Logistics and Courier Services"; 
+    res.locals.siteEmail = "info@zegnetlogistics.com";
+    res.locals.deliveryEmail = "delivery@zegnetlogistics.com";
     res.locals.siteNumber = "+1 (315) 231-0187";
-    res.locals.siteLink = "crestnetlogistics.com";
-    res.locals.adminEmail = "admin@crestnetlogistics.com";
+    res.locals.siteLink = "zegnetlogistics.com";
+    res.locals.adminEmail = "admin@zegnetlogistics.com";
     res.locals.siteAddress = "120 Stuyvesant place, System island 10302 NY USA";  
     
     next()
